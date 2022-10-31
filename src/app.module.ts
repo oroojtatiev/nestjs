db029d92b514a0d8df31974504c750dfbe5ecd1d
oruj.tatiev <oruj.tatiev@gmail.com>
2022-10-31 23:09:07 +0300

    Sequelize removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import {Module} from '@nestjs/common'
 import {AppConfigModule} from './config/config.module'
-import {DatabaseModule} from './database/database.module'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
 
 @Module({
   imports: [
     AppConfigModule,
-    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
