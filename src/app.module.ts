ced2ffb7968872a58916566ba94fc4bf10df0c4f
oruj.tatiev <oruj.tatiev@gmail.com>
2022-11-01 11:23:10 +0300

    Database added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import {Module} from '@nestjs/common'
 import {AppConfigModule} from './config/config.module'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
+import DatabaseModule from './database/database/database.module'
 
 @Module({
   imports: [
     AppConfigModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/config/config.validation.ts
+++ src/config/config.validation.ts
@@ -3,12 +3,16 @@ import * as Joi from 'joi'
 export const schema = Joi.object({
   NODE_ENV: Joi.string().required().valid('development', 'production'),
   APP_PORT: Joi.number().required(),
-  DB_TYPE: Joi.string().required(),
   DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
   DB_NAME: Joi.string().required(),
   DB_USER: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
-  DB_SYNC: Joi.string().required().valid('true', 'false'),
+  TYPEORM_ENTITIES: Joi.string().required(),
+  TYPEORM_SYNC: Joi.string().required().valid('true', 'false'),
   AUTH_JWT_SECRET: Joi.string().required(),
   AUTH_JWT_TOKEN_EXPIRES: Joi.string().required(),
+  PGADMIN_LISTEN_PORT: Joi.string().required(),
+  PGADMIN_DEFAULT_EMAIL: Joi.string().required(),
+  PGADMIN_DEFAULT_PASSWORD: Joi.string().required(),
 })
