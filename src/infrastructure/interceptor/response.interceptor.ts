78b28cb1c64c8b815014af56a170e7d72485346a
oruj.tatiev <oruj.tatiev@gmail.com>
2022-11-02 23:23:24 +0300

    Global infrastructure added

---
+++ src/infrastructure/interceptor/response.interceptor.ts
@@ -0,0 +1,34 @@
+import {Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpStatus} from '@nestjs/common'
+import {Observable} from 'rxjs'
+import {map} from 'rxjs/operators'
+import {isEmptyObject} from '../../functions/helper'
+
+export interface IResponse<T> {
+  status: HttpStatus
+  data?: T | undefined
+  message?: string | undefined
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, IResponse<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<IResponse<T>> {
+    const status = context.switchToHttp().getResponse().statusCode
+    let message: string | undefined = undefined
+
+    return next.handle().pipe(map(project => {
+      if (project === undefined) return {status}
+
+      if (project.message) message = project.message
+
+      delete project.message
+
+      if (isEmptyObject(project)) project = undefined
+
+      return {
+        status,
+        data: project,
+        message: message,
+      }
+    }))
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import {NestFactory} from '@nestjs/core'
 import {ConfigService} from '@nestjs/config'
 import {AppModule} from './app.module'
+import {AppExceptionFilter} from './infrastructure/exception/http-exception.filter'
+import {ResponseInterceptor} from './infrastructure/interceptor/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
+
+  app.useGlobalInterceptors(new ResponseInterceptor())
+  app.useGlobalFilters(new AppExceptionFilter())
+
   await app.listen(configService.get('APP_PORT'))
 }
 
